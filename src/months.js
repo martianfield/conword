b788f68c9a798419b088a4b98a1a623ca15cdefa
Martin Imfeld <martin.imfeld@gmail.com>
2016-09-09 15:56:55 +0200

    attempt to make it work with UglifyJs

--- src/months.js
+++ src/months.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const monthNames = {
   en: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -10,43 +10,43 @@ const monthNames = {
 
 module.exports = function (months, options) {
   // fix options
-  options = options ? options : { language:'en', zeroBased:false, connector:'-' }
-  let zeroBased = options.zeroBased ? options.zeroBased : false
-  let connector = options.connector ? options.connector : '-'
-  let language = options.language ? options.language : 'en'
-  language = monthNames.hasOwnProperty(language) ? language : 'en'
+  options = options ? options : { language:'en', zeroBased:false, connector:'-' };
+  let zeroBased = options.zeroBased ? options.zeroBased : false;
+  let connector = options.connector ? options.connector : '-';
+  let language = options.language ? options.language : 'en';
+  language = monthNames.hasOwnProperty(language) ? language : 'en';
 
   // default result
   let result = {
     months: [],
     periods: []
-  }
+  };
 
   // transform months to matrix
-  let matrix = []
-  let allYear = true
-  let modificator = zeroBased ? 0 : 1
+  let matrix = [];
+  let allYear = true;
+  let modificator = zeroBased ? 0 : 1;
   for(let i=0; i<12; i+=1) {
     if(months.find(month => { return month === i + (modificator)})) {
-      matrix.push(true)
+      matrix.push(true);
     }
   else {
-      matrix.push(false)
-      allYear = false
+      matrix.push(false);
+      allYear = false;
     }
   }
 
   // available month names
   for( let i=0; i<12; i+=1) {
     if(matrix[i]) {
-      result.months.push(monthNames[language][i])
+      result.months.push(monthNames[language][i]);
     }
   }
 
   // periods
   let periods = []
   if(allYear) {
-    periods.push({start:0, end:11})
+    periods.push({start:0, end:11});
   }
   else {
     let currentPeriod = {}
@@ -55,27 +55,27 @@ module.exports = function (months, options) {
       if(monthSelected) {
         // start a new period
         if(currentPeriod.start === undefined) {
-          currentPeriod.start = i
+          currentPeriod.start = i;
         }
         // end the period if we are in december
         if(i === 11) {
-          currentPeriod.end = i
-          periods.push({start:currentPeriod.start, end: currentPeriod.end})
+          currentPeriod.end = i;
+          periods.push({start:currentPeriod.start, end: currentPeriod.end});
         }
       }
       else {
         // end a period?
         if(currentPeriod.start !== undefined) {
-          currentPeriod.end = i-1
-          periods.push({ start:currentPeriod.start, end:currentPeriod.end})
-          currentPeriod = {}
+          currentPeriod.end = i-1;
+          periods.push({ start:currentPeriod.start, end:currentPeriod.end});
+          currentPeriod = {};
         }
       }
     }
     // check if the last period crossed the year boundary ... combine the first and last period into one
     if(matrix[11] && matrix[0]) {
-      let first = periods.shift()
-      periods[periods.length-1].end = first.end
+      let first = periods.shift();
+      periods[periods.length-1].end = first.end;
     }
   }
 
@@ -85,12 +85,12 @@ module.exports = function (months, options) {
     let period = {
       start: monthNames[language][periods[i].start],
       end: monthNames[language][periods[i].end]
-    }
+    };
     if(period.start !== period.end) {
-      result.periods.push(`${period.start}${connector}${period.end}`)
+      result.periods.push(`${period.start}${connector}${period.end}`);
     }
     else {
-      result.periods.push(`${period.start}`)
+      result.periods.push(`${period.start}`);
     }
   }
 
